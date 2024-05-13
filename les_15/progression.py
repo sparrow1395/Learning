2736f85f6c5a4b914f90a29ff762a99b54609925
sparrow1395 <167238371+sparrow1395@users.noreply.github.com>
2024-05-13 19:25:51 +0300

    new

--- les_15/progression.py
+++ les_15/progression.py
@@ -3,32 +3,22 @@ import time
 start = int(input("Enter start number: "))
 prog = int(input("Enter progression: "))
 
-
-class MyClass:
-
-    def __init__(self, start, prog):
-        self.start = start
-        self.prog = prog
-        self.first = None
-
-    def __next__(self):
-        if self.first is None:
-            self.first = self.start
-            return self.first
-        else:
-            self.first *= prog
-            return self.first
-
-    def __iter__(self):
-        return self
+def progression (start, prog):
+    current = 0
+    if current == 0:
+         current = start
+         yield current
+    while True:
+        current *= prog
+        yield current
 
 
-my_list = MyClass(start, prog)
+generator = progression(start, prog)
 
 while True:
     try:
-        for item in my_list:
-            print(item)
-            time.sleep(1)
+        print(next(generator))
+        time.sleep(1)
     except KeyboardInterrupt:
         print("Not so fast, I WANT MORE NUMBERS HEHEHEHEH")
+
